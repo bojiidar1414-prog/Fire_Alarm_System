b0fe2fc9dd37665207319fb3c3e67110a30497bd
bojiidar1414-prog <bojiidar.1414@gmail.com>
2026-05-02 08:29:26 +0300

    Add temperature monitoring with LED and buzzer alerts

---
+++ alram.ino
@@ -0,0 +1,34 @@
+int sensorPin = A0;
+int redLed = 6;
+int greenLed = 7;
+int buzzer = 8;
+
+void setup()
+{
+  pinMode(redLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+}
+
+void loop()
+{
+  int value = analogRead(sensorPin);
+
+  float voltage = value * 5.0 / 1023.0;
+  float temp = (voltage - 0.5) * 100.0;
+
+  if(temp > 40)
+  {
+    digitalWrite(redLed, HIGH);
+    digitalWrite(greenLed, LOW);
+    tone(buzzer, 1000);
+  }
+  else
+  {
+    digitalWrite(redLed, LOW);
+    digitalWrite(greenLed, HIGH);
+    noTone(buzzer);
+  }
+
+  delay(500);
+}
